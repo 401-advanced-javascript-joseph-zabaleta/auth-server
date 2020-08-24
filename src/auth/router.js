d9b08e3c4eb57eeb0c9b2de024b7109874aa5214
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-24 12:03:49 -0700

    Added dependencies, index and server linked

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/** 3rd Party */
+const express = require('express');
+const router = express.Router();
+
+
+/** Local */
+
+
+
+router.post('/signup', signUp)
--- src/server.js
+++ src/server.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+function start(port) {
+    app.listen(port, () => {
+       console.log(`Server running on PORT: ${port}`);
+    });
+};
+
+module.exports = {
+    server: app,
+    start: start,
+};
