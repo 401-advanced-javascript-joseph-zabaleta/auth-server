198f93585c3a73dde61769da4674eaadccc8672d
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-25 09:40:43 -0700

    some typo fixes

--- __tests__/src/auth/models/users/user-models.test.js
+++ __tests__/src/auth/models/users/user-models.test.js
@@ -1,11 +1,13 @@
 'use strict';
 
 require('@code-fellows/supergoose');
+const jwt = require('jsonwebtoken');
 
 const user = require('../../../../../src/auth/models/users/users-models.js');
 
 let testUser1 = { username: 'admin', password: 'password' };
 let testUser2 = { username: 'bob', password: 'password' };
+let SECRET = 'theSuperSecretMessage';
 
 describe('Testing Users Model Functionality: ', () => {
 
@@ -39,7 +41,6 @@ describe('Testing Users Model Functionality: ', () => {
 
 
     it('Should return true for authorized user during validation check', async () => {
-        let newUser = await user.create(testUser2);
 
         let results = await user.authenticateUser('admin', 'password');
 
@@ -48,11 +49,30 @@ describe('Testing Users Model Functionality: ', () => {
     });
 
     it('Should return false for unauthorized user during validation check', async () => {
-        let newUser = await user.create(testUser2);
 
         let results = await user.authenticateUser('FakeUser', 'fakePassword');
 
         expect(results).toBeFalsy();
+    });
+
+    it('Should generate a JWT for a user', async () => {
+        let newUser = await user.create(testUser1);
+
+        let token = await user.generateToken(newUser.username);
+
+        let success = jwt.verify(token, SECRET);
+
+        expect(success).toBeDefined();
+    });
+
+    it('Should compare passwords properly', async () => {
+        let newUser = await user.create(testUser1);
+
+        let plainPassword = 'password'
+
+        let compared = await user.comparePassword(plainPassword, newUser.password);
+
+        console.log(compared);
     });
 
 });
